76696a5f892f8d8e17c8f07eb5f2f456ae97555c
Vidya Keshavan <vidyakeshavan91@gmail.com>
2018-06-12 16:05:29 -0400

    changes to break

--- src/components/Break.vue
+++ src/components/Break.vue
@@ -52,39 +52,6 @@
       </div>
     </div>
 
-<!-- 
-        <div class="row p4one mb2">
-          <div class="two thirds column mb2 csgoalimg">
-           <img src="../assets/img/breakgoal.png" align="top">
-          </div>
-        </div>       
-
-       <div class="row p4one mb2">
-          <div class="six columns">
-             <b> The Original Goal </b> <br> 
-            break. is a mobile application I have been
-            conceptualizing throughout my final semester at
-            Parsons. This was conceived to help users monitor 
-            their device use and encourage them break their 
-            phone-related time management issues.
-          </div>
-        </div>
-
-       <div class="row p4one">
-          <div class="two thirds column persona">
-              <img src="../assets/img/Userpersona.png" align="top">
-         </div>
-         </div>   
-
-
-      </div> -->
-        
-
-          <!-- <div class="six columns cshypothesis">
-         <!-- <i> Stress levels in teens are 
-          higher than ever because of the quantity and quality of 
-          time spent on their smartphones. </i> 
-          </div> --> 
         </div> 
           
       </div>
