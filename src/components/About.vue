6a6279f08a8b780c4fae10d10af00896f135f2ea
Vidya Keshavan <vidyakeshavan91@gmail.com>
2018-06-22 10:59:20 -0400

    about adjust

--- src/components/About.vue
+++ src/components/About.vue
@@ -32,8 +32,8 @@
 
               <!-- <br> <br> -->
 
-              It's as simple as this: If my product design practice can as effectively make complicated 
-              information and services more accessible to users the way <a href="https://www.youtube.com/watch?v=27HIPU7tUaY&feature=youtu.be&t=4">Anthony Bourdain can use 
+              It's as simple as this: If my product design practice can make complicated 
+              information and services more accessible to users as effectively as the way <a href="https://www.youtube.com/watch?v=27HIPU7tUaY&feature=youtu.be&t=4">Anthony Bourdain can use 
                 three-day-old halibut</a> to explain what a Collateralized Debt Obligation is, then I've more than done my job.
                 <br> <br>
 
