9ec2666f82ca58e9266b8d3cf6be011672c10265
Vidya Keshavan <vidyakeshavan91@gmail.com>
2019-03-05 07:22:29 -0500

    disable for now

--- src/router/index.js
+++ src/router/index.js
@@ -1,57 +1,57 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Work from '@/components/Work';
-import minARal from '@/components/minARal';
-import About from '@/components/About';
-import Break from '@/components/Break';
-import Palcare from '@/components/Palcare';
-import Unknown from '@/components/Unknown';
-import Palliativecare from '@/components/Palliativecare';
-// import { VueElevator } from 'vue-elevator'
-// Vue.component('VueElevator', VueElevator);
+// import Vue from 'vue';
+// import Router from 'vue-router';
+// import Work from '@/components/Work';
+// import minARal from '@/components/minARal';
+// import About from '@/components/About';
+// import Break from '@/components/Break';
+// import Palcare from '@/components/Palcare';
+// import Unknown from '@/components/Unknown';
+// import Palliativecare from '@/components/Palliativecare';
+// // import { VueElevator } from 'vue-elevator'
+// // Vue.component('VueElevator', VueElevator);
 
-import vue from 'vue'
-import BackToTop from 'vue-backtotop';
-Vue.use(BackToTop);
+// import vue from 'vue'
+// import BackToTop from 'vue-backtotop';
+// Vue.use(BackToTop);
 
-Vue.use(Router);
+// Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Work',
-      component: Work,
-    },
-    {
-      path: '/minARal',
-      name: 'minARal',
-      component: minARal,
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About,
-    },
-    {
-      path: '/break',
-      name: 'Break',
-      component: Break,
-    },
-    {
-      path: '/palcare',
-      name: 'Palcare',
-      component: Palcare,
-    },
-    {
-      path: '/unknown',
-      name: 'Unknown',
-      component: Unknown,
-    },
-    {
-      path: '/palliativecare',
-      name: 'Palliativecare',
-      component: Palliativecare,
-    },
-  ],
-});
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Work',
+//       component: Work,
+//     },
+//     {
+//       path: '/minARal',
+//       name: 'minARal',
+//       component: minARal,
+//     },
+//     {
+//       path: '/about',
+//       name: 'About',
+//       component: About,
+//     },
+//     {
+//       path: '/break',
+//       name: 'Break',
+//       component: Break,
+//     },
+//     {
+//       path: '/palcare',
+//       name: 'Palcare',
+//       component: Palcare,
+//     },
+//     {
+//       path: '/unknown',
+//       name: 'Unknown',
+//       component: Unknown,
+//     },
+//     {
+//       path: '/palliativecare',
+//       name: 'Palliativecare',
+//       component: Palliativecare,
+//     },
+//   ],
+// });
